5bdf9e229b0dc68723b1a88976631c19771edb40
Niklas Vejlby Schjoldager <nikl1565@stud.kea.dk>
2021-06-15 23:31:15 +0200

    Added NPM module QR-scanner & new html, sass files for select-table

--- scripts/home/order.js
+++ scripts/home/order.js
@@ -1,71 +1,71 @@
 import { paymentMethod } from "./payment_method";
 
 export const basket = {
-  "Ruined Childhood": false,
-  "El Hefe": false,
-  GitHop: false,
-  "Row 26": false,
-  "Hollaback Lager": false,
-  "Hoppily Ever After": false,
-  Sleighride: false,
-  Mowintime: false,
-  Steampunk: false,
-  "Fairy Tale Ale": false,
+    "Ruined Childhood": false,
+    "El Hefe": false,
+    GitHop: false,
+    "Row 26": false,
+    "Hollaback Lager": false,
+    "Hoppily Ever After": false,
+    Sleighride: false,
+    Mowintime: false,
+    Steampunk: false,
+    "Fairy Tale Ale": false,
 };
 
 export function pressingOrder() {
-  localStorage.clear();
-  console.log(localStorage);
+    localStorage.clear();
+    console.log(localStorage);
 
-  document.querySelectorAll(".pay_alert_message").forEach((msg) => {
-    msg.classList.add("hidden");
+    document.querySelectorAll(".pay_alert_message").forEach((msg) => {
+        msg.classList.add("hidden");
 
-    if (Object.keys(basket).every((k) => !basket[k])) {
-      document.querySelector(".pay_alert_message:nth-child(4)").classList.remove("hidden");
-    } else if (Object.keys(paymentMethod).every((k) => !paymentMethod[k])) {
-      document.querySelector(".pay_alert_message").classList.remove("hidden");
-    } else if (paymentMethod.mobilepay) {
-      document.querySelector(".pay_alert_message:nth-child(3)").classList.remove("hidden");
-    } else if (paymentMethod.contactless) {
-      document.querySelector(".pay_alert_message:nth-child(3)").classList.remove("hidden");
-    } else if (paymentMethod.card) {
-      findBasketItems();
-      window.location.href = "form.html";
-    }
-  });
+        if (Object.keys(basket).every((k) => !basket[k])) {
+            document.querySelector(".pay_alert_message:nth-child(4)").classList.remove("hidden");
+        } else if (Object.keys(paymentMethod).every((k) => !paymentMethod[k])) {
+            document.querySelector(".pay_alert_message").classList.remove("hidden");
+        } else if (paymentMethod.mobilepay) {
+            document.querySelector(".pay_alert_message:nth-child(3)").classList.remove("hidden");
+        } else if (paymentMethod.contactless) {
+            document.querySelector(".pay_alert_message:nth-child(3)").classList.remove("hidden");
+        } else if (paymentMethod.card) {
+            findBasketItems();
+            window.location.href = "select-table.html";
+        }
+    });
 }
 
 function findBasketItems() {
-  const basketItems = [];
+    const basketItems = [];
 
-  document.querySelectorAll(".added_beers li").forEach((element) => {
-    let elementName = element.dataset.field;
-    let elementAmount = document.querySelector(`.${element.className} .basket_amount`).value;
-    console.log(elementName);
+    document.querySelectorAll(".added_beers li").forEach((element) => {
+        let elementName = element.dataset.field;
+        let elementAmount = document.querySelector(`.${element.className} .basket_amount`).value;
+        console.log(elementName);
 
-    let items = JSON.parse(localStorage.getItem("itemsArray")) || [];
-    let item = {
-      name: elementName,
-      amount: elementAmount,
-    };
+        let items = JSON.parse(localStorage.getItem("itemsArray")) || [];
+        let item = {
+            name: elementName,
+            amount: elementAmount,
+        };
 
-    items.push(item);
-    basketItems.push(item);
+        items.push(item);
+        basketItems.push(item);
 
-    localStorage.setItem("itemsArray", JSON.stringify(items));
+        localStorage.setItem("itemsArray", JSON.stringify(items));
 
-    console.log(JSON.parse(localStorage.getItem("itemsArray")));
-  });
-
-  setPaymentAmount(basketItems);
-
-  function setPaymentAmount(items) {
-    let totalPrice = 0;
-
-    items.forEach((item) => {
-      totalPrice += item.amount * 40;
+        console.log(JSON.parse(localStorage.getItem("itemsArray")));
     });
 
-    localStorage.setItem("paymentAmount", JSON.stringify(totalPrice));
-  }
+    setPaymentAmount(basketItems);
+
+    function setPaymentAmount(items) {
+        let totalPrice = 0;
+
+        items.forEach((item) => {
+            totalPrice += item.amount * 40;
+        });
+
+        localStorage.setItem("paymentAmount", JSON.stringify(totalPrice));
+    }
 }
