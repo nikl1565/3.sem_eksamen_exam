7e263dc00e69d1e42f9d1b33e089dad88ec36c51
Niklas Vejlby Schjoldager <nikl1565@stud.kea.dk>
2021-06-19 17:55:33 +0200

    Added custom table names, detection and modal text for error + success

--- scripts/select-table/index.js
+++ scripts/select-table/index.js
@@ -2,16 +2,82 @@ import "../../sass/index.scss";
 import QrScanner from "qr-scanner";
 QrScanner.WORKER_PATH = "../../node_modules/qr-scanner/qr-scanner-worker.min.js";
 
-// Webcam / Camera scan
+const settings = {
+    videoElement: document.querySelector("video"),
+    tables: [
+        {
+            qrId: "qrzu",
+            tableNumber: 1,
+        },
+        {
+            qrId: "oiuy",
+            tableNumber: 2,
+        },
+        {
+            qrId: "hgfd",
+            tableNumber: 3,
+        },
+        {
+            qrId: "lkjh",
+            tableNumber: 4,
+        },
+        {
+            qrId: "qwer",
+            tableNumber: 5,
+        },
+    ],
+    modal: {
+        template: document.querySelector(".t-modal"),
+        success: {
+            icon: "modal-success-icon.svg",
+            title: "Success",
+            subtitle: "Your table is",
+            button: "Go to payment",
+        },
+        error: {
+            icon: "modal-error-icon.svg",
+            title: "Table not found",
+            subtitle: "Please try again",
+            button: "Try again",
+        },
+    },
+};
 
-// const videoElement = document.querySelector("video");
+document.addEventListener("DOMContentLoaded", start);
 
-// const qrScanner = new QrScanner(videoElement, (result) => console.log("decoded qr code:", result));
-// qrScanner.start();
+function start() {
+    startQrScanner();
+}
 
-// // Image scan
-// const image = "../../qr-test.png";
+function startQrScanner() {
+    // Grab video element where the camera / video feed is going to be
+    const videoElement = document.querySelector("video");
+    // Create new QrScanner
+    const qrScanner = new QrScanner(videoElement, (result) => qrCodeDetected(result, qrScanner));
+    qrScanner.start();
+}
 
-// QrScanner.scanImage(image)
-//     .then((result) => console.log(result))
-//     .catch((error) => console.log(error || "No QR code found."));
+function qrCodeDetected(qrCode, qrScanner) {
+    const tableList = settings.tables;
+
+    // Check if qrCode is a table that exists in the list (settings)
+    const isQrCodeATable = tableList.find((table) => table.qrId === qrCode);
+
+    if (isQrCodeATable) {
+        prepareModal("success");
+        console.log(`Your table number is: ${isQrCodeATable.tableNumber}`);
+        qrScanner.stop();
+        return;
+    }
+
+    prepareModal("error");
+}
+
+function prepareModal(status) {
+    const modal = settings.modal.template.content.cloneNode(true);
+    const text = settings.modal[status];
+
+    modal.querySelector("modal__button").textContent = text.button;
+
+    console.log(modal);
+}
